0b994efeac5b3c9b6f1c6000d656bdb641b0f2e9
noamzadik17 <124884490+noamzadik17@users.noreply.github.com>
2023-02-21 15:35:09 +0200

    Add files via upload

---
+++ js/buttons.js
@@ -0,0 +1,9 @@
+export function declareBtns() {
+  let burger_btn = document.querySelector("#burger_btn");
+  let nav_open = document.querySelector("#nav_open");
+  burger_btn.addEventListener("click", function () {
+    (nav_open.style.display != "block") ? nav_open.style.display = "block" : nav_open.style.display = "none";
+  })
+}
+
+declareBtns();
